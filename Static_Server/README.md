2c5c497511486861538f76e1cf4de135e631d1b4
Xue QiaoLing <i-xueqiaoling@mobike.com>
2018-11-13 22:48:52 +0800

    chore(route && mime)

--- Static_Server/README.md
+++ Static_Server/README.md
@@ -2,8 +2,16 @@
 
 The NodeJS Static Web Server
 
+### Introduction
+1. 显示跟目标下的所有文件，点击相应的文件进入（显示文件夹/文件内容）;
+2. 实现根据文件的扩展名显示正确的 Content-Type;
+3. 对传输的内容进行压缩；
+4.
+
 ### How to use
 
 1. install: `npm -g Static_Server`;
 2.
+
+
 
--- Static_Server/src/utils/route.js
+++ Static_Server/src/utils/route.js
@@ -1,4 +1,13 @@
 const fs = require('fs');
+const path = require('path');
+const Handlebars = require('handlebars');
+const config = require('../config/defaultConfig');
+const mime = require('./mime');
+
+// 在引入文件时，除了 require()中一般使用相对路径，其他情况尽量使用绝对路径；
+const tplPath = path.join(__dirname, '../index.html');
+const source = fs.readFileSync(tplPath); // buffer 同步读文件，因为拿到模板文件是下面的前提，同时又可以利用缓存，不用每次都读取模板文件
+const template =  Handlebars.compile(source.toString());
 
 /**
  * 根据路由判断指向的内容是一个文件 or 文件夹
@@ -13,6 +22,8 @@ module.exports = function route(filePath, req, res) {
     }
 
     if(stats.isFile()) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', mime(filePath));
       // fs.readFile(filePath, (data) => {
       //   res.end(data);
       // }); 相应较慢 需要读完所有的内容才会返回结果，而不是像流一样，读到一部分就输出这部分结果
@@ -20,8 +31,19 @@ module.exports = function route(filePath, req, res) {
     } else if(stats.isDirectory()) {
       fs.readdir(filePath, (err, files) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(files.join(','));
+        res.setHeader('Content-Type', 'text/html');
+        const dir = path.relative(config.root, filePath);
+        const data = {
+          files: files.map((file) => {
+            return {
+              icon: mime(file),
+              file: file
+            }
+          }),
+          dir: dir ? `/${dir}` : '', // 注意需要加上根路径/ 表示相对于根路径的绝对路径
+          title: path.basename(filePath)
+        };
+        res.end(template(data));
       });
     }
   });
